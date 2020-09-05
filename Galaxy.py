f07d912c95f2fe4a474aaae9452e368a53084d32
Abhijit Panchal <37021060+abhi-j@users.noreply.github.com>
2020-09-05 20:53:03 +0530

    Update Galaxy.py
    follow the instructions  has been changed

--- Galaxy.py
+++ Galaxy.py
@@ -1,31 +1,47 @@
 from tkinter import *
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
+#main window 
+main_win = Tk()
 
-root = Tk()
+#size of the window
+main_win.geometry("1800x960")
 
-root.geometry("4930x3346")
+#background image
+bg = PhotoImage(file="lost_in_space.png")
+PhotoImage = Label(image= bg,bg = "black",height = 1000,width = 1800).place(x = 0,y = 0)
 
-root.title("Galaxy")
+#Title of the Game
+Label(main_win, text = 'GALAXY',fg = "black", bg="red", font =("orbitron", 100)).place(x = 520,y = 200)
 
-image1 = ImageTk.PhotoImage(Image.open(r"C:\Users\Abhijit\Desktop\board game.png"))
-label = Label(image = image1)
-label.pack()
+#defining message for buttons
+def inst():
+    messagebox.showinfo("Help", "Follow the instructions,
+                        The game starts from “Start” at bottom left and end at “Got the crystal” The game can be played with one or more than one player, 
+                        ➢ The player can move by rolling the dice, 
+                        ➢ The player has to move according the number on the dice, 
+                        ➢ After the first player rolls the dice, second should get the chance to roll the dice and move his token, 
+                        ➢ The game has special cards with some tasks written, when a player lands on a tile without a number on a special tile, 
+                        The card asks to accomplish a task, if the player accepts the task he can move. If he does not accept the card, he should 10 steps back,
+                        ➢ The player wins the game if the token lands on the last tile with text")
+    
+#Creating a new window
+def playgame():
+    global game_bg
+    game_wn = Toplevel()
+    game_wn.geometry("1800x960")
+    game_wn.title("Galaxy")
+    game_bg = ImageTk.PhotoImage(Image.open("board game-Recovered (1).png"))
+    my_label = Label(game_wn,image = game_bg).pack()
 
-def click():
-    label = Label(root, text = "Hello")
-    label.pack()
 
-def instruction():
-    inst = Label(root, text = """ """)
+    
+#creating buttons
+button1 = Button(main_win, text = "Play Game",command = playgame, fg = "white", bg = "orange", 
+                 padx = 50, pady = 5,font =("orbitron", 15)).place(x = 800, y = 480)
+button2 = Button(main_win, text = "Instructions",command = inst,fg = "white", bg = "orange", 
+                 padx = 42, pady = 5, font =("orbitron", 15)).place(x = 800, y = 580)
+button3 = Button(main_win, text = "Exit Game",command = main_win.destroy, fg = "white", bg = "orange",
+                 padx = 55, pady = 5, font =("orbitron", 15)).place(x = 800, y = 680)
 
-    inst.pack()
-Button1 = Button(root, text = "Play Game", command = click, pady = 10, padx = 25, fg = "white", bg = "orange")
-Button2 = Button(root, text = "Instructions", command = instruction, pady = 10, padx = 22, fg = "white", bg = "orange")
-# Button3 = Button(root, text = "", command = click, pady = 10, padx = 25)
-Button4 = Button(root, text = "Exit", command = click, pady = 10, padx = 44, fg = "white", bg = "orange")
-Button1.pack()
-Button2.pack()
-# Button3.pack()
-Button4.pack()
-
-root.mainloop()
+#main event loop fro screen
+main_win.mainloop()
